75bd0c0dd7c8ba9cb905bd92f5208286ab29d11a
Kisaragi <kisaragi.z@hacarus.com>
2022-12-08 20:55:14 +0900

    modified docstring

--- modules/dataset.py
+++ modules/dataset.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from pathlib import Path
 import pandas as pd
 
+from typing import Tuple
+
 @dataclass
 class WineDataset:
     """Dataset for wine quality data."""
@@ -10,8 +12,20 @@ class WineDataset:
     def __post_init__(self) -> None:
         self.data = pd.read_csv(self.data_dir)
 
-    def split_data(self, train_ratio: float):
-        """Split data into train and test set."""
+    def split_data(self, train_ratio: float) -> Tuple:
+        """Split data into train and test set.
+        
+        Return
+        -----------
+        X_train : np.array
+            Independent variables from train set.
+        X_test : np.array
+            Independent variables from test set.
+        y_train : np.array
+            dependent variables from train set.
+        y_test : np.array
+            dependent variables from test set.
+        """
         X_train = self.data.drop("quality", axis=1).sample(frac=train_ratio, random_state=20221207)
         X_test = self.data.drop("quality", axis=1).drop(X_train.index)
 
