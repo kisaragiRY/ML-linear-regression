58bec8b20187050b0b9a10bafda367366b25ac6c
Kisaragi <none>
2023-01-28 11:51:28 +0800

    return prediction instead of property

--- modules/trainer.py
+++ modules/trainer.py
@@ -1,3 +1,5 @@
+from numpy.typing import NDArray
+
 import numpy as np
 from scipy.linalg import inv
 from dataclasses import dataclass
@@ -7,16 +9,50 @@ class LinearRegression:
     """A linear regression model.
     """
     def __post_init_(self) -> None:
+        """Initiate.
+        """
         self.fitted_coeff = None
 
-    def fit(self, X_train: np.array, y_train: np.array) -> None:
-        """Train the model."""
-        self.fitted_coeff = (inv(X_train.T @ X_train) @ X_train.T).dot(y_train)
+    def fit(self, X_train: NDArray, y_train: NDArray, k: float = 0) -> None:
+        """Train the model.
         
-    
-    def predict(self, X_test: np.array) -> None:
-        """Make inference."""
+        Parameter
+        ---
+        X_train: NDArray
+            the design matrix from the training set.
+        y_train: NDArray
+            the response variable from the training set.
+        k: float
+            the Ridge parameter, 0 if the OLS(ordinary least square) is implemented.
+        """
+        self.__is_valid_k(k)
+        n_coeff = X_train.shape[1]
+
+        if k == 0:
+            try:
+                inv(X_train.T @ X_train)
+                self.fitted_coeff = (inv(X_train.T @ X_train) @ X_train.T).dot(y_train)
+            except:
+                raise ValueError("Design matrix is singular.")
+        else:
+            self.fitted_coeff = (inv(X_train.T @ X_train + k * np.identity(n_coeff)) @ X_train.T).dot(y_train)
+
+        
+    def __is_valid_k(self, k: float):
+        """Check if the Ridge parameter k is valid.
+        """
+        if k < 0:
+            raise ValueError("Ridge parameter k must be larger than 0.")
+
+    def predict(self, X_test: NDArray) -> None:
+        """Make inference.
+        
+        Parameter
+        ---
+        X_test: NDArray
+            Design matrix from the test set.
+        """
         if self.fitted_coeff is not None:
-            self.prediction = X_test.dot(self.fitted_coeff)
+            return X_test.dot(self.fitted_coeff)
         else:
             raise ValueError("Model is not fitted yet.")
